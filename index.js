fe7c236f3b192ee3dc09a34af73aa3044c7e8a0c
miguelorlandos06 <miguelorlandos06@gmail.com>
2026-09-13 19:08:53 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const path = require('path');
 const crypto = require('crypto');
 const { pipeline } = require('stream/promises');
 
-const BOT_TOKEN = "8633754852:AAFI3Eesq3dy2MqSWFGndJ7MDrm4eVHHlpc";
+const BOT_TOKEN = "8935664196:AAFePCRuzC27eP4nb2LWTvoYaCWnQ60LMSE";
 const S3 = "https://s3.todus.cu/stream";
 const DOWNLOAD_PATH = "/tmp/todus_uploads";
 
